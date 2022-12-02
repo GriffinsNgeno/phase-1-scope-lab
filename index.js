f44583b8989890034b334ca7c8d858d553f7aef7
GriffinsNgeno <griffins.ngeno@student.moringaschool.com>
2022-12-02 12:48:29 +0300

    Assignment Completed

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Write your solution in this file!
+// Declaring Customer name variable
+var customerName = 'bob';
+
+// Function to modify cunstomerName to upperCase
+function upperCaseCustomerName() {
+    customerName = customerName.toUpperCase();
+}
+// Set best customer
+function setBestCustomer() {
+    bestCustomer = 'not bob';
+}
+
+// Overwrites best customer
+function overwriteBestCustomer() {
+    bestCustomer = 'maybe bob';
+}
+
+// Outputs an error
+const leastFavoriteCustomer = 'Number'
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = 'bob'
+}
